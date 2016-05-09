286cc724a27d489bfe14103833e4e9e83b9fed68
王策 <634692517@qq.com>
2016-05-09 20:25:24 +0800

    优化了 Constraint.swift

--- FangYuan/Constraint.swift
+++ FangYuan/Constraint.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-/// 判断两个约束是否产生了循环依赖
 // TODO: 可是关于方向的问题你有没有想明白？
+
 infix operator <=> {}
+/// 判断两个约束是否产生了循环依赖
 func <=>(lhs: Constraint, rhs: Constraint) -> Bool {
     return lhs.to == rhs.from && lhs.from == rhs.to
 }
@@ -24,7 +25,8 @@ class Constraint: Hashable {
 
     let hashValue: Int
 
-    static var ConstraintHash = 0
+    /// 为满足 Constraint: Hashable，每次 Constraint.init 执行后，hashStore 值会 +1
+    static var hashStore = 0
 
     /// 约束来源于那个视图
     weak var from: UIView!
@@ -48,14 +50,22 @@ class Constraint: Hashable {
         case RightLeft
         case TopBottom
     }
-
+    
+    /**
+     初始化方法
+     
+     - parameter from:      该约束需要等待 from 布局完成后才能是有效的
+     - parameter to:        from 布局完成后，to会开始根据这个约束来布局
+     - parameter direction: 约束方向
+     - parameter value:     该约束的固定值，默认为 0
+     */
     init(from: UIView?, to: UIView?, direction: Constraint.Direction, value: CGFloat = 0) {
-        Constraint.ConstraintHash += 1
-        self.hashValue = Constraint.ConstraintHash
+        self.hashValue = Constraint.hashStore
         self.from = from
         self.to = to
         self.direction = direction
         self.value = value
+        Constraint.hashStore += 1
     }
 }
 
