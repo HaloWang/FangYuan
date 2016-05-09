71a2597982421a61f8e74604c94d93ea1a71b0a0
王策 <634692517@qq.com>
2016-05-09 18:05:43 +0800

    创建了四个方向上的 holder 以针对 `fy_edge` 方法

--- FangYuan/ConstraintManager.swift
+++ FangYuan/ConstraintManager.swift
@@ -23,9 +23,41 @@ class ConstraintManager {
     // TODO: 可以把这个集合变成多叉树，以便更有针对性的进行 map/filter
     /// 全部约束
     var constraints = Set<Constraint>()
-
-    /// 刚刚压入的约束
-    var constraintHolder: Constraint?
+    
+    class ConstraintHolder {
+        var topBottom: Constraint?
+        var bottomTop: Constraint?
+        var leftRight: Constraint?
+        var rightLeft: Constraint?
+        
+        func constraintAt(direction: Constraint.Direction) -> Constraint? {
+            switch direction {
+            case .TopBottom:
+                return topBottom
+            case .BottomTop:
+                return bottomTop
+            case .LeftRigt:
+                return leftRight
+            case .RightLeft:
+                return rightLeft
+            }
+        }
+        
+        func set(constraint:Constraint?, at direction:Constraint.Direction) {
+            switch direction {
+            case .TopBottom:
+                topBottom = constraint
+            case .BottomTop:
+                bottomTop = constraint
+            case .LeftRigt:
+                leftRight = constraint
+            case .RightLeft:
+                rightLeft = constraint
+            }
+        }
+    }
+    
+    var holder = ConstraintHolder()
 
     /// 未设定约束相关信息
     var unsetConstraintInfo: (has: Bool, constraints: [Constraint]) {
@@ -46,7 +78,8 @@ extension ConstraintManager {
      - parameter direction: 约束方向
      */
     class func getConstraintFrom(from:UIView, direction: Constraint.Direction) {
-        singleton.constraintHolder = Constraint(from: from, to: nil, direction: direction)
+        let newConstraint = Constraint(from: from, to: nil, direction: direction)
+        singleton.holder.set(newConstraint, at: direction)
     }
 
     // TODO: setConstraint 是生成『渲染队列』的最佳时机了吧
@@ -61,7 +94,7 @@ extension ConstraintManager {
      - parameter value:     约束固定值
      */
     class func setConstraintTo(to:UIView, direction: Constraint.Direction, value:CGFloat) {
-        guard let holder = singleton.constraintHolder else {
+        guard let _constraint = singleton.holder.constraintAt(direction) else {
             return
         }
         
@@ -70,11 +103,11 @@ extension ConstraintManager {
         // TODO: 未实现
         singleton.removeAndWarningCyclingConstraint()
 
-        holder.to = to
-        holder.value = value
+        _constraint.to = to
+        _constraint.value = value
 
-        singleton.constraints.insert(holder)
-        singleton.constraintHolder = nil
+        singleton.constraints.insert(_constraint)
+        singleton.holder.set(nil, at: direction)
     }
 
     class func layout(view:UIView) {
