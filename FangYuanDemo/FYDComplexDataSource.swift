a51fae1047132ced4e653b7ffb6ba342db7a048b
王策 <634692517@qq.com>
2016-05-25 17:29:53 +0800

    Code optimize

--- FangYuanDemo/FYDComplexDataSource.swift
+++ FangYuanDemo/FYDComplexDataSource.swift
@@ -13,17 +13,24 @@ class FYDComplexDataSource {
     ]
     
     static var randomData : [Item] {
+        
+        func randomString(maxRepeat:Int, repeatString:String) -> String {
+            return Array(count: (Int(arc4random()) % maxRepeat) + 1, repeatedValue: repeatString).reduce("") { $0 + $1 }
+        }
+        
         var items = [Item]()
         for _ in 0 ..< (Int(arc4random()) % 50) + 50 {
             let item       = Item()
-            item.nickName  = Array(count: (Int(arc4random()) % 3) + 1, repeatedValue: "昵称").reduce("") { $0 + $1 }
-            item.message   = Array(count: (Int(arc4random()) % 40) + 1, repeatedValue: "动态内容").reduce("") { $0 + $1 }
+            item.nickName  = randomString(3, repeatString: "昵称")
+            item.message   = randomString(40 , repeatString: "动态内容")
             item.avatar    = ""
             item.imageURLs = Array(count: (Int(arc4random()) % 10), repeatedValue: "")
             item.isMine    = (arc4random() % 4 == 0)
+            
             for _ in 0..<Int(arc4random()) % 5 {
-                item.comments.append(Array(count: (Int(arc4random()) % 20), repeatedValue: "评论").reduce("") { $0 + $1 })
+                item.comments.append(randomString(10, repeatString: "评论内容"))
             }
+            
             items.append(item)
         }
         return items
