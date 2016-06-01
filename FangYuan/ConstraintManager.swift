7ebb3a058031b61d50e45cde121458708819398f
王策 <634692517@qq.com>
2016-06-01 20:09:47 +0800

    no message

--- FangYuan/ConstraintManager.swift
+++ FangYuan/ConstraintManager.swift
@@ -86,6 +86,8 @@ extension ConstraintManager {
     /// - Important: 这里面已经产生了递归调用了：fy_XXX -> [This Method] -> fy_XXX -> [This Method] -> ...
     /// - TODO: 部分方法不应该遍历两次的！这里的性能还有提升空间
     /// - TODO: horizontal 的意义并不明显啊
+    /// - TODO: 这种『分 - 总』式的方法，是完全可以使用并发遍历的，顺便也可以考量一下 `Set.remove` 的多线程可用性
+    /// - TODO: 可以写一个 UT 来测试一下并发遍历和非并发遍历
     class func resetRelatedConstraintFrom(view:UIView, isHorizontal horizontal:Bool) {
         singleton.settedConstraints.forEach { constraint in
             if let _from = constraint.from {
